7f92a36be2c1d67c943de86e58963cf10e830aef
palavraeserie7 <Palavraeserie@gmail.com>
2026-05-14 20:09:32 -0300

    Update supabase.js

--- js/services/supabase.js
+++ js/services/supabase.js
@@ -1,18 +1,13 @@
-// ======================================
-// SUPABASE GLOBAL CLIENT
-// Palavra em Série V9
-// ======================================
+const SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
 
-const SUPABASE_URL = 'https://qmhdbuxomozdczgvpvcc.supabase.co';
-
-const SUPABASE_ANON_KEY = 'sb_publishable_5jVrL8dyVBg0YOfo_q4ppA_yLMOjTz8';
+const SUPABASE_ANON_KEY =
+"sb_publishable_5jVrL8dyVBg0YOfo_q4ppA_yLMOjTz8";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-// Disponibiliza globalmente
 window.supabaseClient = supabaseClient;
 
-console.log("✅ Supabase conectado.");
+console.log("Supabase OK");
