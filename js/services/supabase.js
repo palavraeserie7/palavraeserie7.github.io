5af0bf5cf4e3deca2af05a6f75e2f42afb44c78f
palavraeserie7 <Palavraeserie@gmail.com>
2026-05-17 00:03:51 -0300

    Update supabase.js

--- js/services/supabase.js
+++ js/services/supabase.js
@@ -1,13 +1,15 @@
-const SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
+const SUPABASE_URL =
+"https://SEU-PROJETO.supabase.co";
 
-const SUPABASE_ANON_KEY =
-"sb_publishable_5jVrL8dyVBg0YOfo_q4ppA_yLMOjTz8";
+const SUPABASE_KEY =
+"SUA_CHAVE";
 
-const supabaseClient = window.supabase.createClient(
+const supabaseClient =
+window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_KEY
 );
 
 window.supabaseClient = supabaseClient;
 
-console.log("Supabase OK");
+console.log("Supabase iniciado");
