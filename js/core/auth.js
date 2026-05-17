efb7a130f9762481281b03d37b20fa15d1d1203d
palavraeserie7 <Palavraeserie@gmail.com>
2026-05-17 16:08:21 -0300

    Update auth.js

--- js/core/auth.js
+++ js/core/auth.js
@@ -1,1 +1,19 @@
+import supabase from "./supabase.js";
 
+export async function checkAuth() {
+
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+
+        window.location.replace(
+            "/pages/login.html"
+        );
+
+        return null;
+    }
+
+    return user;
+}
