8f6f9e0fed22bf5722b07c4143b2a0c02bb66db1
palavraeserie7 <Palavraeserie@gmail.com>
2026-05-17 16:09:42 -0300

    Update permissions.js

--- js/core/permissions.js
+++ js/core/permissions.js
@@ -1,1 +1,40 @@
+export function renderBook(
+    title,
+    description,
+    isPremium = false
+) {
 
+    return `
+        <div class="book-card">
+
+            <span class="tag ${
+                isPremium
+                ? "pro-tag"
+                : "free-tag"
+            }">
+                ${
+                    isPremium
+                    ? "PREMIUM"
+                    : "FREE"
+                }
+            </span>
+
+            <h3>${title}</h3>
+
+            <p>${description}</p>
+
+            <button class="${
+                isPremium
+                ? "btn-bloqueado"
+                : "btn-liberado"
+            }">
+                ${
+                    isPremium
+                    ? "BLOQUEADO"
+                    : "LER AGORA"
+                }
+            </button>
+
+        </div>
+    `;
+}
