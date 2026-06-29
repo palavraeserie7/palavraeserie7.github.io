1e27434f3c721f7083a868604fa87a0fcddb582e
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 16:07:25 -0300

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,1 +1,22 @@
 
+import { supabase } from "./supabase.js";
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const msg = document.getElementById("msg");
+  msg.innerText = "Entrando...";
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    msg.innerText = error.message;
+    return;
+  }
+
+  window.location.href = "./dashboard.html";
+};
