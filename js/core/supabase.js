bc56390b443f9fad64785f123e8a7dfa9e2c62f5
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 15:48:30 -0300

    Update supabase.js

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -1,14 +1,1 @@
-const SUPABASE_URL =
-  "https://qmhdbuxomozdczgvpvcc.supabase.co";
 
-const SUPABASE_KEY =
-  "sb_publishable_5jVrL8dyVBg0YOfo_q4ppA_yLMOjTz8";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-
-window.supabaseClient = supabase;
-
-export default supabase;
