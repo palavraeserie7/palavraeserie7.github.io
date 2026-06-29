fd619c01cb0eb106f70391957347c0fce4736f48
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 16:12:32 -0300

    Update orchestrator.js

--- js/core/orchestrator.js
+++ js/core/orchestrator.js
@@ -1,15 +1,1 @@
-import { checkMaintenanceStatus }
-from "./maintenance.js";
 
-window.addEventListener(
-    "DOMContentLoaded",
-    async () => {
-
-        console.log(
-            "ORCHESTRATOR OK"
-        );
-
-        await checkMaintenanceStatus();
-
-    }
-);
