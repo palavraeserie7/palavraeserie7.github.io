7656475287a53c814a51b5dfe3d8c4c0ffbb27f6
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 15:50:52 -0300

    Update supabase.js

--- services/supabase.js
+++ services/supabase.js
@@ -1,13 +1,1 @@
-import { createClient }
-from "@supabase/supabase-js";
 
-const supabase =
-createClient(
-
-process.env.SUPABASE_URL,
-
-process.env.SUPABASE_SERVICE_ROLE
-
-);
-
-export default supabase;
