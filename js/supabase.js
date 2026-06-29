df7497b2e9a4264712b7a0288078401b8e81c746
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 15:58:27 -0300

    Update and rename js/core/auth.js to js/supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
+const SUPABASE_KEY = "SUA_ANON_KEY_AQUI";
+
+export const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
