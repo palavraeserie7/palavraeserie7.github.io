68f6b850714ed37ff90f3a132e00cea1c0bc9247
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 17:19:28 -0300

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-
-const SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
-const SUPABASE_KEY = "SUA_ANON_KEY_AQUI";
-
-export const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
