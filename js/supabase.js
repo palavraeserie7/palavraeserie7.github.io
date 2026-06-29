41c7396c2d1601acc3b1c97e602d9001c306eff4
palavraeserie7 <Palavraeserie@gmail.com>
2026-06-29 17:25:04 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://qmhdboxomozdczgvpvcc.supabase.co";
+const SUPABASE_KEY = "SUA_ANON_KEY_AQUI";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
