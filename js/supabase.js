c37ab30cb17b7bcfb96b29384d6babd791636a7b
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-11 15:44:27 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,7 @@
-// Configuração do Supabase
-var SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
-var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ"; // <-- COLE SUA CHAVE 'anon public' AQUI
+// Configuração correta do Supabase
+var SUPABASE_URL = "https://qmhdduxomozdczgvpvcc.supabase.co"; 
+var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ"; 
 
-// Previne erro de redeclaração caso o script carregue duas vezes
 if (!window.supabaseClientInstance ) {
     window.supabaseClientInstance = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 }
