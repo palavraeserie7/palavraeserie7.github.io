a413433873b742105ce7c0370aacd85e02d0ac2b
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-11 17:26:27 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
-// Configuração DEFINITIVA do Supabase
 var SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co"; 
-var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ"; 
+var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ"; // Ela deve ser bem longa!
 
 if (!window.supabaseClientInstance ) {
     window.supabaseClientInstance = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
