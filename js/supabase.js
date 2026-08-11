2196a225f67ce00e3f024c8efa1ce83330821ad2
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-11 15:42:15 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase
 var SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
-var SUPABASE_KEY = "sb_publishable_5jVrL8dyVBg0YOfo_q4ppA_yLMOjTz8"; // <-- COLE SUA CHAVE 'anon public' AQUI
+var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ"; // <-- COLE SUA CHAVE 'anon public' AQUI
 
 // Previne erro de redeclaração caso o script carregue duas vezes
 if (!window.supabaseClientInstance ) {
