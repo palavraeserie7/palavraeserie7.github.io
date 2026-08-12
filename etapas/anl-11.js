f3691a8f1d600e633bdaab847a8be91e2575b239
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:43:24 -0300

    Create anl-11.js

---
+++ etapas/anl-11.js
@@ -0,0 +1,20 @@
+export const anl11 = {
+    id: "ANL-11",
+    titulo: "TEOLOGIA E COMPARAÇÃO",
+    itens: [
+        "Teologia sistemática",
+        "Coerência doutrinária",
+        "Comparação de interpretações",
+        "Concordâncias entre fontes",
+        "Divergências entre fontes",
+        "Argumentos das diferentes interpretações",
+        "Consenso acadêmico",
+        "Posições minoritárias",
+        "Evidências favoráveis e contrárias",
+        "Grudem",
+        "Berkhof",
+        "Frame",
+        "Evangelical Dictionary of Theology",
+        "Comentários bíblicos em comparação"
+    ]
+};
