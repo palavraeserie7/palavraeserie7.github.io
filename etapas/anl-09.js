0229f1474e919e206b9fa24736f3f6cb04c8cdfd
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:42:28 -0300

    Create anl-09.js

---
+++ etapas/anl-09.js
@@ -0,0 +1,21 @@
+export const anl09 = {
+    id: "ANL-09",
+    titulo: "RELAÇÃO COM O RESTANTE DA ESCRITURA",
+    itens: [
+        "Relações AT–NT",
+        "Citações",
+        "Alusões",
+        "Ecos",
+        "Paralelos",
+        "Uso do AT no NT",
+        "LXX e NT",
+        "Relações canônicas",
+        "Desenvolvimento da revelação",
+        "História da redenção",
+        "Beale",
+        "Beale & Carson",
+        "Vos",
+        "Goldsworthy",
+        "Schreiner"
+    ]
+};
