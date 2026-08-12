ecaccf45cd045bdf2c778f2e2629bf00a99eb244
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:38:50 -0300

    Create anl-03.js

---
+++ etapas/anl-03.js
@@ -0,0 +1,19 @@
+export const anl03 = {
+    id: "ANL-03",
+    titulo: "GRAMÁTICA E SINTAXE",
+    itens: [
+        "Morfologia",
+        "Gramática hebraica",
+        "Gramática aramaica",
+        "Gramática grega",
+        "Sintaxe hebraica",
+        "Sintaxe grega",
+        "Estrutura das frases",
+        "Verbos",
+        "Tempos e aspectos",
+        "Voz",
+        "Modo",
+        "Relações sintáticas",
+        "Conectivos"
+    ]
+};
