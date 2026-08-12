89dcf7c2eb5838e86828fb669378d5d1d5cf23f0
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 02:02:15 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 /**
- * SUPABASE CONNECTOR V8.1
+ * SUPABASE CONNECTOR V8.2 - ARQUITETURA MASTER
  */
 var SUPABASE_URL = "https://qmhdbuxomozdczgvpvcc.supabase.co";
 var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRidXhvbW96ZGN6Z3ZwdmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NDg3NzEsImV4cCI6MjA5NDAyNDc3MX0.AElEyUr0J57k0gm6sjRPawq6Iipbuv-eVrRjUU8TlZQ";
@@ -7,4 +7,5 @@ var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSI
 if (!window.supabaseClientInstance ) {
     window.supabaseClientInstance = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 }
+// O objeto 'supabase' fica disponível para o M00 usar internamente
 var supabase = window.supabaseClientInstance;
