ec76ba9958ae3b44ea2cc8855f3c358dfabde962
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:41:32 -0300

    Create anl-07.js

---
+++ etapas/anl-07.js
@@ -0,0 +1,25 @@
+export const anl07 = {
+    id: "ANL-07",
+    titulo: "CONTEXTO HISTÓRICO, CULTURAL E RELIGIOSO",
+    itens: [
+        "Contexto histórico",
+        "Período histórico",
+        "Cultura",
+        "Costumes",
+        "Sociedade",
+        "Instituições",
+        "Religião",
+        "Práticas religiosas",
+        "Grupos religiosos",
+        "Templos",
+        "Sacerdócio",
+        "Festas",
+        "Crenças",
+        "Zondervan Encyclopedia",
+        "IVP Bible Background Commentary",
+        "New Bible Dictionary",
+        "ANET",
+        "Context of Scripture",
+        "Manners and Customs"
+    ]
+};
