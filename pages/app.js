52ab30148646796edbe31b848ef3455882bd1cae
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:19:39 -0300

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    for (const [key, value] of Object.entries(matrizData)) {
-        const section = document.createElement('section');
-        section.className = 'etapa';
-        section.innerHTML = `
-            <h2>${key} — ${value.titulo}</h2>
-            <ul>
-                ${value.itens.map(item => `<li>${item}</li>`).join('')}
-            </ul>
-        `;
-        app.appendChild(section);
-    }
-});
+
