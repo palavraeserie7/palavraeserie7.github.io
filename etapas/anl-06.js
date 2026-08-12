27e5b6dd3cad8233e2791446f0ac9c94310ba2d6
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:40:48 -0300

    Create anl-06.js

---
+++ etapas/anl-06.js
@@ -0,0 +1,23 @@
+export const anl06 = {
+    id: "ANL-06",
+    titulo: "EXEGESE E CONTEXTO LITERÁRIO",
+    itens: [
+        "Contexto imediato",
+        "Contexto da passagem",
+        "Estrutura literária",
+        "Argumento do autor",
+        "Intenção comunicativa",
+        "Gênero literário",
+        "Relação entre versículos",
+        "Exegese versículo a versículo",
+        "Comentários bíblicos",
+        "BECNT",
+        "NIGTC",
+        "NICNT",
+        "Pillar",
+        "WBC",
+        "NICOT",
+        "AOTC",
+        "Baker OT"
+    ]
+};
