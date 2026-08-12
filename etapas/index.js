64d495458807bc0c6e769e8c80b48427b09f2c32
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:48:53 -0300

    Create index.js

---
+++ etapas/index.js
@@ -0,0 +1,27 @@
+import { anl01 } from './anl-01.js';
+import { anl02 } from './anl-02.js';
+import { anl03 } from './anl-03.js';
+import { anl04 } from './anl-04.js';
+import { anl05 } from './anl-05.js';
+import { anl06 } from './anl-06.js';
+import { anl07 } from './anl-07.js';
+import { anl08 } from './anl-08.js';
+import { anl09 } from './anl-09.js';
+import { anl10 } from './anl-10.js';
+import { anl11 } from './anl-11.js';
+import { anl12 } from './anl-12.js';
+
+export const matrizCompleta = {
+    "ANL-01": anl01,
+    "ANL-02": anl02,
+    "ANL-03": anl03,
+    "ANL-04": anl04,
+    "ANL-05": anl05,
+    "ANL-06": anl06,
+    "ANL-07": anl07,
+    "ANL-08": anl08,
+    "ANL-09": anl09,
+    "ANL-10": anl10,
+    "ANL-11": anl11,
+    "ANL-12": anl12
+};
