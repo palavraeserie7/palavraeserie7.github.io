3e105522c77e3a6229362742ab1cf24f3162efde
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:43:57 -0300

    Create anl-12.js

---
+++ etapas/anl-12.js
@@ -0,0 +1,16 @@
+export const anl12 = {
+    id: "ANL-12",
+    titulo: "SÍNTESE",
+    itens: [
+        "Integração das evidências",
+        "Conclusão exegética",
+        "O que o texto afirma",
+        "O que o texto implica",
+        "O que é interpretação",
+        "O que é inferência",
+        "O que é aplicação",
+        "Questões ainda debatidas",
+        "Grau de segurança da conclusão",
+        "Síntese final para geração de conteúdo"
+    ]
+};
