4b900f17ba6821726ade92f9fed4a6bcbdc5c485
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:26:51 -0300

    Create anl-01.js

---
+++ pages/etapas/anl-01.js
@@ -0,0 +1,17 @@
+export const anl01 = {
+    id: "ANL-01",
+    titulo: "TEXTO E MANUSCRITOS",
+    itens: [
+        "Texto hebraico",
+        "Texto aramaico",
+        "Texto grego",
+        "Manuscritos",
+        "Testemunhos textuais",
+        "Variantes textuais",
+        "Crítica textual",
+        "NA28",
+        "BHS / BHQ",
+        "SBLGNT",
+        "Septuaginta"
+    ]
+};
