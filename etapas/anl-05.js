4608b3e551d31d2044ea10e46281a8ccdf628c34
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:40:16 -0300

    Create anl-05.js

---
+++ etapas/anl-05.js
@@ -0,0 +1,15 @@
+export const anl05 = {
+    id: "ANL-05",
+    titulo: "SIGNIFICADO TEOLÓGICO",
+    itens: [
+        "Desenvolvimento dos conceitos",
+        "Vocabulário teológico",
+        "Uso teológico no AT",
+        "Uso teológico no NT",
+        "Relação com a LXX",
+        "NIDNTTE",
+        "NIDOTTE",
+        "TDNT",
+        "TDOT"
+    ]
+};
