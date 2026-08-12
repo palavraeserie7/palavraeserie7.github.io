08fa29c5ee535bfa611f656c43266fe480113e5b
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:41:59 -0300

    Create anl-08.js

---
+++ etapas/anl-08.js
@@ -0,0 +1,24 @@
+export const anl08 = {
+    id: "ANL-08",
+    titulo: "CONTEXTO GEOGRÁFICO E POLÍTICO",
+    itens: [
+        "Localização geográfica",
+        "Cidades",
+        "Regiões",
+        "Territórios",
+        "Fronteiras",
+        "Rotas",
+        "Montanhas",
+        "Rios",
+        "Reinos",
+        "Impérios",
+        "Governantes",
+        "Estruturas políticas",
+        "Guerras",
+        "Conflitos",
+        "Relações de poder",
+        "Arqueologia",
+        "Atlas bíblico",
+        "Zondervan Atlas"
+    ]
+};
