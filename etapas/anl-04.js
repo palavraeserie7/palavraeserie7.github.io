434254460ae8ccc0a710ba6c2da1fe991ed0f004
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-12 20:39:50 -0300

    Create anl-04.js

---
+++ etapas/anl-04.js
@@ -0,0 +1,17 @@
+export const anl04 = {
+    id: "ANL-04",
+    titulo: "PALAVRAS E SEMÂNTICA",
+    itens: [
+        "Palavras-chave",
+        "Lemas",
+        "Formas lexicais",
+        "Campo semântico",
+        "Sentido contextual",
+        "Uso no AT",
+        "Uso no NT",
+        "BDAG",
+        "HALOT",
+        "BDB",
+        "LSJ"
+    ]
+};
