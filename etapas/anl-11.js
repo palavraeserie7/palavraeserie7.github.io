92361ae3d612eb09230178c6de8b223f12b2c860
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:16:49 -0300

    Update anl-11.js

--- etapas/anl-11.js
+++ etapas/anl-11.js
@@ -1,5 +1,2 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js';
-export async function runANL11(alvo) {
-    return await executarANL("ANL-11", "Teologia e Comparação", ["Grudem", "Berkhof", "Frame", "EDT"], 
-        `Confronte as evidências obtidas com a teologia sistemática e história da interpretação para: '${alvo}'.`);
-}
+
+import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run11(alvo, contexto) { return await executarANL("ANL-11", "Teologia e Comparação", ["Grudem", "Berkhof"], "Confronte evidências com a teologia sistemática de: " + alvo, contexto); }
