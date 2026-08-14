dc4b116fe26cfad8858c4abeb9a5bc83ee28c56b
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:15:57 -0300

    Update anl-09.js

--- etapas/anl-09.js
+++ etapas/anl-09.js
@@ -1,5 +1,1 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js';
-export async function runANL09(alvo) {
-    return await executarANL("ANL-09", "Relação com o Restante da Escritura", ["Beale", "Beale & Carson", "Vos", "Goldsworthy"], 
-        `Analise citações, alusões, ecos e a história da redenção para: '${alvo}'.`);
-}
+import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run09(alvo, contexto) { return await executarANL("ANL-09", "Relação Canônica", ["Beale", "Vos"], "Analise a intertextualidade de: " + alvo, contexto); }
