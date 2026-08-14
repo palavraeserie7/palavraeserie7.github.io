ba268913e69039cf57d620d4b24f941410e0b973
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:12:46 -0300

    Update anl-06.js

--- etapas/anl-06.js
+++ etapas/anl-06.js
@@ -1,5 +1,1 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js';
-export async function runANL06(alvo) {
-    return await executarANL("ANL-06", "Exegese e Contexto Literário", ["BECNT", "NICNT", "NICOT", "Pillar", "WBC"], 
-        `Determine a intenção comunicativa, gênero e contexto literário imediato para: '${alvo}'.`);
-}
+import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run06(alvo, contexto) { return await executarANL("ANL-06", "Exegese e Contexto Literário", ["BECNT", "NICNT", "NICOT"], "Determine a intenção do autor e o contexto literário de: " + alvo, contexto); }
