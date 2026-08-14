06dcb602b17dc502be91fee32b747808cb609060
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:38:46 -0300

    Update anl-04.js

--- etapas/anl-04.js
+++ etapas/anl-04.js
@@ -1,1 +1,5 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run04(alvo, contexto) { return await executarANL("ANL-04", "Palavras e Semântica", ["BDAG", "HALOT", "BDB"], "Analise o campo semântico das palavras-chave de: " + alvo, contexto); }
+
+import { executarANL } from '../pages/pesquisa/js/engine.js';
+export async function run01(alvo, contexto) {
+    return await executarANL("ANL-01", "Texto", ["NA28", "BHS"], "Analise o texto para: " + alvo, contexto);
+}
