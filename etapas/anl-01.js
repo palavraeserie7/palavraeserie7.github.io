7a3b6b9b2f57129399bd96dd015a9a8d2e9d0e57
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:37:40 -0300

    Update anl-01.js

--- etapas/anl-01.js
+++ etapas/anl-01.js
@@ -1,2 +1,5 @@
 
-import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run01(alvo, contexto) { return await executarANL("ANL-01", "Texto e Manuscritos", ["NA28", "BHS", "Septuaginta"], "Estabeleça o texto crítico para: " + alvo, contexto); }
+import { executarANL } from '../pages/pesquisa/js/engine.js';
+export async function run01(alvo, contexto) {
+    return await executarANL("ANL-01", "Texto", ["NA28", "BHS"], "Analise o texto para: " + alvo, contexto);
+}
