5ee5b55d634cb2203a569e6532cecade98d22b13
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 00:03:33 -0300

    Update anl-01.js

--- etapas/anl-01.js
+++ etapas/anl-01.js
@@ -1,9 +1,6 @@
 
-export const ANL_01 = {
-    etapa: 1,
-    nome: "Texto Original",
-    fontes: ["NA28", "BHS/BHQ", "SBLGNT", "Septuaginta", "Textus Receptus"],
-    funcao: "Estabelecimento do texto grego crítico ou hebraico.",
-    prompt: (tema) => `Etapa 1: Analise o texto original (Grego ou Hebraico) para o tema '${tema}'. Cite as edições críticas NA28 ou BHS/BHQ.`
-};
-
+import { executarANL } from '../pages/pesquisa/js/engine.js';
+export async function runANL01(alvo) {
+    return await executarANL("ANL-01", "Texto e Manuscritos", ["NA28", "BHS", "BHQ", "Septuaginta", "Metzger"], 
+        `Estabeleça o texto crítico e testemunhos textuais para: '${alvo}'.`);
+}
