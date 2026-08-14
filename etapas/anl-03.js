10c6b881998c04835acc85fffc86b958daad1f8c
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:38:23 -0300

    Update anl-03.js

--- etapas/anl-03.js
+++ etapas/anl-03.js
@@ -1,1 +1,4 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run03(alvo, contexto) { return await executarANL("ANL-03", "Gramática e Sintaxe", ["Wallace", "Joüon"], "Analise a morfologia e sintaxe para: " + alvo, contexto); }
+import { executarANL } from '../pages/pesquisa/js/engine.js';
+export async function run01(alvo, contexto) {
+    return await executarANL("ANL-01", "Texto", ["NA28", "BHS"], "Analise o texto para: " + alvo, contexto);
+}
