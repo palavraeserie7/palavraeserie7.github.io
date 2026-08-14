747bfc6bd2b5ed0aa8bbe06cef50a671a57ce804
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 01:16:23 -0300

    Update anl-10.js

--- etapas/anl-10.js
+++ etapas/anl-10.js
@@ -1,5 +1,1 @@
-import { executarANL } from '../pages/pesquisa/js/engine.js';
-export async function runANL10(alvo) {
-    return await executarANL("ANL-10", "Hermenêutica e Controle", ["Osborne", "Carson", "Fee & Stuart"], 
-        `Faça o controle metodológico contra eisegese, falácias lexicais e anacronismos para: '${alvo}'.`);
-}
+import { executarANL } from '../pages/pesquisa/js/engine.js'; export async function run10(alvo, contexto) { return await executarANL("ANL-10", "Hermenêutica e Controle", ["Osborne", "Carson"], "Faça o controle hermenêutico para: " + alvo, contexto); }
