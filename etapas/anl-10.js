7a4e4604eef889cd132dc6b566f9874e709eeb75
palavraeserie7 <Palavraeserie@gmail.com>
2026-08-14 00:06:55 -0300

    Update anl-10.js

--- etapas/anl-10.js
+++ etapas/anl-10.js
@@ -1,7 +1,5 @@
-export const ANL_10 = {
-    etapa: 10,
-    nome: "Teologia Sistemática",
-    fontes: ["Grudem", "Berkhof", "Frame", "EDT"],
-    funcao: "Validação doutrinária e síntese sistemática.",
-    prompt: (tema) => `Etapa 10: Realize uma síntese de teologia sistemática sobre '${tema}' usando Grudem ou Berkhof.`
-};
+import { executarANL } from '../pages/pesquisa/js/engine.js';
+export async function runANL10(alvo) {
+    return await executarANL("ANL-10", "Hermenêutica e Controle", ["Osborne", "Carson", "Fee & Stuart"], 
+        `Faça o controle metodológico contra eisegese, falácias lexicais e anacronismos para: '${alvo}'.`);
+}
